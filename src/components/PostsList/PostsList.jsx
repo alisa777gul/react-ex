fac427502c5386b98f880647eb4dd5cc64128c71
aliso4ka <gulyayevaalisa@gmail.com>
2025-02-03 14:36:18 +0100

    gf

--- src/components/PostsList/PostsList.jsx
+++ src/components/PostsList/PostsList.jsx
@@ -2,16 +2,22 @@ import { useSelector } from 'react-redux';
 import { selectAllPosts } from '../../features/posts/postsSlice.js';
 import css from './PostsList.module.css';
 import PostAuthor from '../PostAuthor/PostAuthor.jsx';
+import TimeAgo from '../TimeAgo/TimeAgo.jsx';
 
 const PostsList = () => {
   const posts = useSelector(selectAllPosts);
 
-  const renderPosts = posts.map(post => (
+  const orderedPosts = posts
+    .slice()
+    .sort((a, b) => b.date.localeCompare(a.date));
+
+  const renderPosts = orderedPosts.map(post => (
     <article className={css.post} key={post.id}>
       <h3>{post.title}</h3>
       <p>{post.content.substring(0, 100)}</p>
       <p className={css.postCredit}>
         <PostAuthor userId={post.userId} />
+        <TimeAgo timestamp={post.date} />
       </p>
     </article>
   ));
--- src/features/posts/postsSlice.js
+++ src/features/posts/postsSlice.js
@@ -1,8 +1,33 @@
 import { createSlice, nanoid } from '@reduxjs/toolkit';
+import { sub } from 'date-fns';
 
 const initialState = [
-  { id: '1', title: 'Learning Redux Toolkit', content: 'Ivarfav aeve' },
-  { id: '2', title: 'Ledux Toolkit', content: 'Ivarfav aeve' },
+  {
+    id: '1',
+    title: 'Learning Redux Toolkit',
+    content: 'Ivarfav aeve',
+    date: sub(new Date(), { minutes: 10 }).toISOString(),
+    reactions: {
+      thumbsUp: 0,
+      wow: 0,
+      heart: 0,
+      rocket: 0,
+      coffee: 0,
+    },
+  },
+  {
+    id: '2',
+    title: 'Ledux Toolkit',
+    content: 'Ivarfav aeve',
+    date: sub(new Date(), { minutes: 5 }).toISOString(),
+    reactions: {
+      thumbsUp: 0,
+      wow: 0,
+      heart: 0,
+      rocket: 0,
+      coffee: 0,
+    },
+  },
 ];
 
 const postsSlice = createSlice({
@@ -19,10 +44,21 @@ const postsSlice = createSlice({
             id: nanoid(),
             title,
             content,
+            date: new Date().toISOString(),
             userId,
+            reactions: {
+              thumbsUp: 0,
+              wow: 0,
+              heart: 0,
+              rocket: 0,
+              coffee: 0,
+            },
           },
         };
       },
+    },
+    reactionAdded(state, action) {
+      const { postId, reaction } = action.payload;
     },
   },
 });
